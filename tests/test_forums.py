6263431a6438a6e6ab87c1b07cac68e7e74213ef
Ademsk1 <adam.illoul24@gmail.com>
2023-01-13 12:36:51 +0000

    finished up testing, 11 tests on posting, editing and deleting posts and comments

--- tests/test_forums.py
+++ tests/test_forums.py
@@ -60,30 +60,73 @@ def test_item_post_no_user():
          item_to_post,
          headers={"Content-type": "application/json"}
          )
-        print(response.json())
         assert response.status_code==200
         assert response.json()[0]['status']==500
 
 def test_item_post_error():
     pass
 def test_item_edited():
+
     pass
-def test_item_deleted():
-    item_to_delete = json.dumps([{'user_id':'1','post_id':'1','user_type':1}])
+def test_add_comment():
+    comment_item = json.dumps([{'post_id':1,'description':'TEST_COMMENT','user_id':1}])
     with app.app_context():
+        last_post = requests.get(base_url+'/forum_post').json()[0]
+        comment_item = json.dumps([{'post_id':last_post['id'],'description':'TEST_COMMENT','user_id':last_post['user_id']}])
+        response = requests.post(base_url +'/forum_comment',data=comment_item,headers={'Content-Type':'application/json'})
+        assert response.status_code==200
+        all_comments = requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()[0]
+        print(all_comments)
+        assert all_comments['description']=='TEST_COMMENT'
+    pass
+def test_edit_comment():
+    with app.app_context():
+        last_post = requests.get(base_url+'/forum_post').json()[0]
+        all_comments = requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()[0]
+        edited_text = 'TEST_COMMENT_EDITED'
+        comment_id = all_comments['id']
+        user_id = all_comments['user_id']
+        data = json.dumps([{'user_id':user_id,'comment_id':comment_id,'description':edited_text}])
+        edited_comment_message = requests.patch(base_url+'/forum_comment',data=data,headers={'Content-Type':'application/json'}).json()
+        print(edited_comment_message)
+        edited_comments = requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()[0]
+        assert edited_comments.get('description')!=None
+        assert edited_comments.get('description')=='TEST_COMMENT_EDITED'
+def test_delete_comment_user():
+        last_post = requests.get(base_url+'/forum_post').json()[0]
+        all_comments = requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()[0]
+        comment_id = all_comments['id']
+        user_id = all_comments['user_id']
+        data = json.dumps([{'comment_id':comment_id,'user_id':user_id,'user_type':2}])
+        response = requests.delete(base_url+'/forum_comment',data=data,headers={'Content-Type':'application/json'})
+        all_comments = requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()
+        print(response.json())
+        print(all_comments)
+        assert len(all_comments)==0
+        assert response.json()[0].get('?column?')!=None
+def test_delete_any_comment_admin():
+    test_item_post()
+    test_add_comment()
+    last_post = requests.get(base_url+'/forum_post').json()[0]
+    all_comments = requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()[0]
+    comment_id = all_comments['id']
+    user_id = 5 #an admin account
+    data = json.dumps([{'comment_id':comment_id,'user_id':user_id,'user_type':1}])
+    response = requests.delete(base_url+'/forum_comment',data=data,headers={'Content-Type':'application/json'})
+    new_comments =  requests.post(base_url+'/get_comments',data=json.dumps([{'post_id':last_post['id']}]),headers={'Content-Type':'application/json'}).json()
+    print(new_comments)
+    assert len(new_comments)==0
+
+    
+
+    pass
+def test_post_deleted():
+        last_post = requests.get(base_url+'/forum_post').json()[0]
+        item_to_delete = json.dumps([{'user_id':last_post['user_id'],'post_id':last_post['id'],'user_type':last_post['user_id']}])
         response = requests.delete(base_url+'/forum_post',
         data=item_to_delete,
         headers={"Content-type": "application/json"})
-        assert response.json()[0]['status']==404 #will fail, if there is no post_id=1
-def test_item_deleted_error():
-    pass
-def test_add_comment():
-    pass
-def test_edit_comment():
-    pass
-def test_delete_comment():
-    pass
-def test_get_all_comments():
-    pass
+        print(response.json()[0])
+        assert response.json()[0].get('?column?')=='post has been deleted successfully'
 
 
